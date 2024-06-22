017a7d43b86ad42d5fc15718ed16a1be79a3003c
tejasgadgil <tejassgadgil@gmail.com>
2024-06-22 23:45:33 +0530

    Initial springboot project

---
+++ src/main/java/com/example/spendwise/SpendWiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.spendwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spendwise/SpendWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spendwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
